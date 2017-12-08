7163e784a21a773f9689966083044632eaa736f8
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-12-08 20:18:54 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     version=__version__,
     packages=find_packages(exclude=['tests.*', 'tests']),
     tests_require=["nose", 'coverage>=4.0,<4.4'],
-    install_requires=map(str, parse_requirements('requirements.txt')),
+    install_requires=open('requirements.txt', 'r').readlines(),
     test_suite='nose.collector',
 )
