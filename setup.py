8ffde99bc0764e1724ef9bd503289163dd650215
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-12-08 20:15:34 +0100

    reqs

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 # coding=UTF-8
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
 
 from firanka import __version__
 
@@ -7,6 +9,6 @@ setup(
     version=__version__,
     packages=find_packages(exclude=['tests.*', 'tests']),
     tests_require=["nose", 'coverage>=4.0,<4.4'],
-    install_requires=open('requirements.txt', 'r').readlines(),
+    install_requires=map(str, parse_requirements('requirements.txt')),
     test_suite='nose.collector',
 )
