3a802e67edc7c54032f3f073aacb327c1cebff12
Piotr Maslanka <piotr.maslanka@henrietta.com.pl>
2017-12-08 17:40:46 +0100

    scaffold

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 # coding=UTF-8
 from setuptools import setup, find_packages
 
-from sai import __version__
+from firanka import __version__
 
 setup(
     version=__version__,
     packages=find_packages(exclude=['tests.*', 'tests']),
-    python_requires='==2.7.*',
     tests_require=['nose', 'mock', 'coverage'],
     test_suite='nose.collector',
-    zip_safe=False
 )
